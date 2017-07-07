716647ef1a27373e377cccf5e44c65247dc12450
Your Name <youremail@domain.com>
2017-07-07 15:41:06 +0300

    added spec for requiring definition in word.rb

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,9 +1,23 @@
 require('rspec')
 require('word')
+require('definition')
 
 describe('word')do
   before()do
-    Word.clear
+    Word.clear()
+  end
+
+describe(".clear")do
+  it("emptys all the saved word instances")do
+    expect(Word.clear()).to(eq([]))
+  end
+end
+
+describe('#save')do
+  it("saves a new word to the words array")do
+  word1 = Word.new("cat")
+  word1.save()
+  expect(Word.all()).to(eq([word1]))
   end
 end
 
@@ -13,16 +27,17 @@ describe('.all')do
     word1.save()
     expect(Word.all()).to(eq([word1]))
   end
-  describe('#save')do
-    it("saves a new word to the words array")do
-    word1 = Word.new("cat")
+end
+
+describe('#add_definition')do
+    it("adds definition to a word")do
+    word1 = Word.new("shoe")
     word1.save()
-    expect(Word.all()).to(eq([word1]))
+    definition1 = Definition.new("Your foot wear.")
+    definition1.save()
+    word1.add_definition(definition1)
+    expect(word1.get_definition()).to(eq([definition1]))
   end
 end
-describe(".clear")do
-  it("emptys all the saved word instances")do
-    expect(Word.clear()).to(eq([]))
-  end
-end
+
 end
