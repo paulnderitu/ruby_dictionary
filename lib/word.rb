04a5877500edef57cd4b1da23a90cf650862a146
Your Name <youremail@domain.com>
2017-07-07 14:13:14 +0300

    added the clearing method to ensure that all is empty at first

--- lib/word.rb
+++ lib/word.rb
@@ -7,4 +7,11 @@ class Word
   define_singleton_method(:all)do
     @@words
   end
+  define_method(:save)do
+    @@words.push(self)
+  end
+  define_singleton_method(:clear)do
+    @@words.
+  end
+end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -7,4 +7,21 @@ describe('.all')do
     word1.save()
     expect(Word.all()).to(eq([word1]))
   end
+  describe('#save')do
+    it("saves a new word to the words array")do
+    word1 = Word.new("cat")
+    word1.save()
+    expect(Word.all()).to(eq([word1]))
+  end
 end
+dwescribe(".clear")do
+  it("emptys all the saved word instances")do
+    expect(Word.clear()).to(eq([]))
+  end
+end
+end
+
+# describe('.clear') do
+#  +    it("is empty at first") do
+#  +      expect(Word.clear()).to(eq([]))
+#  +    end
