74f45c14c8a9e626ef07dcf8cae1d2e7260d884a
Your Name <youremail@domain.com>
2017-07-07 17:56:06 +0300

    updated the dictionary integration pec and app.rb to know what to expect once integrated

---
+++ spec/dictionary_integration_spec.rb
@@ -0,0 +1,35 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+
+describe('path of index', {:type=> :feature}) do
+  it("takes you to success page when you add a word") do
+    visit('/')
+    click_button('Add!')
+    expect(page).to have_content("Congratulations! Your word has been added successfully")
+  end
+end
+
+describe('path of word list at index', {:type=> :feature}) do
+  it("prints out a list of words added at index page") do
+    visit('/')
+    fill_in("word", :with=> "house")
+    click_button('Add!')
+    visit('/')
+    expect(page).to have_content("house")
+  end
+end
+
+describe('path of word', {:type=> :feature}) do
+    it("takes you to a one word page") do
+    visit('/word/:id')
+    expect(page).to have_content("Add your own definition")
+  end
+end
+
+describe('path of definition', {:type=> :feature}) do
+  it("runs success page after definition input") do
+    visit('/success_2')
+    expect(page).to have_content("Definition Added:")
+  end
+end
