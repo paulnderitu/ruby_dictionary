6158d470a537349a40602bb08d6a2510be1e64c2
Your Name <youremail@domain.com>
2017-07-07 11:33:33 +0300

    initialized the attributes of the wprd and the definition to be pushed in their respective arrrays

--- lib/word.rb
+++ lib/word.rb
@@ -1,5 +1,7 @@
-require('rspec')
-require
-describe('word')do
-  expect
+class Word
+  attr_reader(:word, :defition)
+  @@words = []
+  define_method(:initialize) do |attributes|
+    @definitions = []
+  end
 end
